f9c54867e1adad66afc773e13aae97fb31562e63
sandeepk75767 <103090237+sandeepk75767@users.noreply.github.com>
2022-04-18 16:05:54 -0400

    Add files via upload

---
+++ FindSecondLargest.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+
+public class FindSecondLargest {
+
+	public static void main(String[] args) {
+		
+		int [] array = new int [] {3,2,11,4,6,7};  
+		
+		Arrays.sort(array);   
+		System.out.println("Elements of array sorted in ascending order: ");  
+		 
+		for (int i = 0; i < array.length; i++)   
+		{       
+		System.out.println(array[i]);
+		
+		}   
+		
+		}  
+		}  
+
+
---
+++ PrimeNumber.java
@@ -0,0 +1,30 @@
+package week1.day1;
+
+public class PrimeNumber {
+
+	public static void main(String[] args) {
+			    int input = 13;
+			  int  count = 0;
+			   
+			    for (int i = 1; i <= input; i++) {
+			      
+			      if (i % 2 == 0) {
+			        count = count+1;
+		
+			      }
+			    }
+
+			    if (count==2)
+			    {
+			    	System.out.println("given number is a prime number.");
+			    }
+			    else
+			    {
+			    	System.out.println("given number is not a prime number.");
+			  }
+			
+
+	}
+}
+
+
