2e80418336c97f2ccf693ba18e920661f9fcd2b9
sandeepk75767 <103090237+sandeepk75767@users.noreply.github.com>
2022-05-02 14:10:27 -0400

    Assignment3(Method Overloading)

--- Calculator.java
+++ Calculator.java
@@ -1,12 +1,12 @@
-/* Sandeep Kaur*/
-package week1.day1;
+package methodOverloadingAssignment;
+
 import java.util.Scanner;
+
 public class Calculator {
 	
-	 
-	 
 	 public static void main(String[] args) {
-	  Scanner in = new Scanner(System.in);
+		 
+	Scanner in = new Scanner(System.in);
 	   
 	  System.out.print("Input first number: ");
 	  int num1 = in.nextInt();
@@ -14,8 +14,9 @@ public class Calculator {
 	  System.out.print("Input second number: ");
 	  int num2 = in.nextInt();
 	   
+	  
 	 
-	  System.out.println(num1 + " + " + num2 + " = " + 
+	  System.out.println(num1 + " + " + num2 + " + "+ "= " + 
 	  (num1 + num2));
 	   
 	  System.out.println(num1 + " - " + num2 + " = " + 
@@ -27,9 +28,9 @@ public class Calculator {
 	  System.out.println(num1 + " / " + num2 + " = " + 
 	  (num1 / num2));
 	 
-	  System.out.println(num1 + " mod " + num2 + " = " + 
-	  (num1 % num2));
+	  
 	 }
 	 
 	}
-	
+
+
